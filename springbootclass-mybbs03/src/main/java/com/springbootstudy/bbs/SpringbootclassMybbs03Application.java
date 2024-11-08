35e34eb95c6066b069a3b5df8a4bc33fcd484810
jangmi <jsy639@naver.com>
2024-11-08 17:18:58 +0900

    GitHub test

---
+++ springbootclass-mybbs03/src/main/java/com/springbootstudy/bbs/SpringbootclassMybbs03Application.java
@@ -0,0 +1,13 @@
+package com.springbootstudy.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootclassMybbs03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootclassMybbs03Application.class, args);
+	}
+
+}
